c517e340d8baca24bd59b611031b785c5db9a54b
Walter Barbieri <walter90@hotmail.it>
2023-08-01 13:51:51 +0200

    Primo Commit

---
+++ proveMattina/w18d2proveMattina/src/main/java/w18d2proveMattina/W18d2proveMattinaApplication.java
@@ -0,0 +1,13 @@
+package w18d2proveMattina;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class W18d2proveMattinaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(W18d2proveMattinaApplication.class, args);
+	}
+
+}
---
+++ proveMattina/w18d2proveMattina/src/test/java/w18d2proveMattina/W18d2proveMattinaApplicationTests.java
@@ -0,0 +1,13 @@
+package w18d2proveMattina;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class W18d2proveMattinaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
